8652eb472c95d937ec8efd6260032b3e6fdb9902
poppylouadams <poppy.louise-adams@nhs.net>
2025-04-30 15:09:25 +0000

    End of course

--- test1pre.workshop.sql
+++ test1pre.workshop.sql
@@ -12,4 +12,25 @@ SELECT
  FROM PatientStay ps
  WHERE ps.Hospital IN ('PRUH', 'Oxleas')
  AND ps.Ward LIKE '%Surgery'
- AND ps.AdmittedDate BETWEEN '2024-02-27' AND '2024-03-01'
+-- AND ps.AdmittedDate BETWEEN '2024-02-27' AND '2024-03-01'
+ORDER BY ps.AdmittedDate desc, ps.PatientId DESC
+
+SELECT
+     ps.Hospital
+    , ps.Ward 
+    , COUNT(*) AS Numberofpatients 
+    , SUM(ps.Tariff) AS Totaltariff
+    , AVG(ps.Tariff) AS AverageTariff
+    , MAX(ps.Tariff) AS MaxTariff
+    , MIN(ps.Tariff) AS MinTariff
+ FROM PatientStay ps
+ GROUP BY ps.Hospital, ps.Ward
+ ORDER BY Numberofpatients DESC
+
+ SELECT  ps.PatientId
+    , ps.AdmittedDate
+    , ps.Hospital
+    , dh.hospitaltype
+    , dh.hospitalsize
+    FROM PatientStay ps LEFT JOIN DimHospitalBad dh ON ps.Hospital = dh.Hospital WHERE DH.Hospital IS NULL
+
